2cbe58b2a4d26f377f98faba33ce59f6de9218c7
padawan <mason.camp@evsck12.com>
2021-11-17 12:22:54 -0600

    mapChange

--- NavApp/app/src/main/java/com/example/navapp/MainActivity.java
+++ NavApp/app/src/main/java/com/example/navapp/MainActivity.java
@@ -9,25 +9,27 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isPressed=false;
+
 
     Button graphicBTN;
-    Button button29;
-    Button button30;
-    Button button31;
-    Button button32;
-    Button button33;
-    Button button34;
-    Button button35;
-    Button button36;
-    Button button37;
-    Button button38;
-    Button button39;
-    Button button40;
-    Button button41;
-    Button button42;
-    Button button43;
-    Button button44;
-    Button button45;
+    Button culinaryBTN;
+    Button dieselBTN;
+    Button automotiveBTN;
+    Button veterinaryBTN;
+    Button collisionBTN;
+    Button radioBTN;
+    Button weldingBTN;
+    Button engineeringBTN;
+    Button csBTN;
+    Button cnBTN;
+    Button hvacBTN;
+    Button safetyBTN;
+    Button precisionBTN;
+    Button manufacturingBTN;
+    Button electricityBTN;
+    Button constructionBTN;
+    Button healthBTN;
     ImageView image;
 
     @Override
@@ -35,35 +37,87 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        graphicBTN=findViewByTd(R.id.button27);
-        Button findViewByTd(R.id.button29);
-        Button findViewByTd(R.id.button30);
-        Button findViewByTd(R.id.button31);
-        Button findViewByTd(R.id.button32);
-        Button findViewByTd(R.id.button33);
-        Button findViewByTd(R.id.button34);
-        Button findViewByTd(R.id.button35);
-        Button findViewByTd(R.id.button36);
-        Button findViewByTd(R.id.button37);
-        Button findViewByTd(R.id.button38);
-        Button findViewByTd(R.id.button39);
-        Button findViewByTd(R.id.button40);
-        Button findViewByTd(R.id.button41);
-        Button findViewByTd(R.id.button42);
-        Button findViewByTd(R.id.button43);
-        Button findViewByTd(R.id.button44);
-        Button findViewByTd(R.id.button45);
+
+        graphicBTN=findViewById(R.id.button27);
+        graphicBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                graphicMap();
+            }
+        });
+        culinaryBTN=findViewById(R.id.button29);
+        dieselBTN=findViewById(R.id.button30);
+        automotiveBTN=findViewById(R.id.button31);
+        veterinaryBTN=findViewById(R.id.button32);
+        collisionBTN=findViewById(R.id.button33);
+        radioBTN=findViewById(R.id.button34);
+        weldingBTN=findViewById(R.id.button35);
+        engineeringBTN=findViewById(R.id.button36);
+        csBTN=findViewById(R.id.button37);
+        cnBTN=findViewById(R.id.button38);
+        hvacBTN=findViewById(R.id.button39);
+        safetyBTN=findViewById(R.id.button40);
+        precisionBTN=findViewById(R.id.button41);
+        manufacturingBTN=findViewById(R.id.button42);
+        electricityBTN=findViewById(R.id.button43);
+        constructionBTN=findViewById(R.id.button44);
+        healthBTN=findViewById(R.id.button45);
+        image=findViewById(R.id.imageView);
 
     }
-    View.OnClickListener buttonListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if(isPressed)
-                button.setBackgroundResource(R.drawable.icon1);
-            else
-                button.setBackgroundResource(R.drawable.icon2);
+    private void graphicMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void collisionMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void csMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void culinaryMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void dieselMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void electricityMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void engineeringMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void healthScienceMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void hvacMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void matinenceMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void networkMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void precisionMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void publicSafetyMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void radioMapMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void weldingMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void buildingTradesMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void graphicMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
+    private void graphicMap(){
+        image.setImageResource(R.drawable.graphicmap);
+    }
 
-            isPressed = !isPressed;
-        }
-    };
 }
